bdccf5e3f80a98a11dbb9db92e84f12b49b779b5
Jordan Shefner <yarden11111@gmail.com>
2020-03-15 19:36:20 +0200

    Enum fix

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   has_many :my_tasks
   has_many :users, through: :my_tasks
-  enum privacy: [ :private, :public ]
+  enum privacy: [ :personal, :general ]
   enum status: [ :not_approved, :approved ]
 end
