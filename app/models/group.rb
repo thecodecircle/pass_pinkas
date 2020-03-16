6ef41006209a009fc6ae7390916ace1caf168685
Jordan Shefner <yarden11111@gmail.com>
2020-03-16 16:58:29 +0200

    Fix infinite validation loop

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,8 +2,8 @@ class Group < ApplicationRecord
   belongs_to            :branch
   validates             :branch, presence: true
 
-  has_many              :my_groups
-  validates_associated  :my_groups
+  has_many              :my_groups, inverse_of: :user
+  # validates_associated  :my_groups
 
   has_many              :users, through: :my_groups
   validates_associated  :users
