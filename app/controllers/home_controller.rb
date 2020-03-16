72a431318c0f526e99ed0a6aaaf64e4e302c3220
Pashi <guynatan@gmail.com>
2020-03-16 17:48:59 +0200

    home esign

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
 		@my_tasks = current_user.tasks.all
 		@random_tasks = Task.where(publicity: 1, status: 1).left_outer_joins(:my_tasks).where( my_tasks: { id: nil } ).limit(5).order("RANDOM()")
+		@done_tasks = @my_tasks.where("my_tasks.progress = ? ", 1)
+		@approved_tasks = @my_tasks.where("my_tasks.progress = ? ", 2)
   end
 end
