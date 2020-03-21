185ad0233275b1ef93b6461ef66e4f5ff10a2fed
Jordan Shefner <yarden11111@gmail.com>
2020-03-21 16:44:50 +0200

    Add dependant destroy

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,11 +2,11 @@ class Region < ApplicationRecord
   belongs_to            :movement
   validates             :movement, presence: true
 
-  has_many              :branches
+  has_many              :branches, dependent: :destroy
   validates_associated  :branches
 
-  has_many              :my_groups, as: :my_groupable
-  has_many              :users, through: :my_groups, as: :my_groupable
+  has_many              :my_groups, as: :my_groupable, dependent: :destroy
+  has_many              :users, through: :my_groups, as: :my_groupable, dependent: :destroy
 
   validates             :name, presence: true
   validates             :score, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
