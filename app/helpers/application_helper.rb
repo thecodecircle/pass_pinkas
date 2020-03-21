665814c6cde6eda035e3d4ac050788d8f200b558
Pashi <guynatan@gmail.com>
2020-03-21 15:15:31 +0200

    bug buttn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+	def mobile_device?
+		 if session[:mobile_param]
+			 session[:mobile_param] == "1"
+		else
+				request.user_agent =~ /Mobile|webOS/
+		end
+	end
+
+	def error_btn
+		if mobile_device?
+			html = "<hr>"
+			html += "<a href='whatsapp://send?text=דיווח על תקלה&phone=972546621300' data-action='share/whatsapp/share' class='bg-yellow-500 hover:bg-yellow-700 text-black py-2 px-4 rounded'> <i class='gg-pen'></i> דיווח על תקלה</a>"
+		else
+			html = "<hr>"
+			html += mail_to 'yarden11111@gmail.com', '<i class="gg-pen"></i> דיווח על תקלה '.html_safe,class:'bg-yellow-500 hover:bg-yellow-700 text-black py-2 px-4 rounded', subject: 'דיווח על תקלה'
+		end
+		html.html_safe
+	end
+
 end
