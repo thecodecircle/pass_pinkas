baa77e6d93fa8030780d6efc873ebb9239878cb3
Pashi <guynatan@gmail.com>
2020-03-29 16:46:36 +0300

    remove task and retrun it to kid

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -86,24 +86,32 @@ class HomeController < ApplicationController
     puts "****************************************"
     puts "Change Progress"
     if params[:kid_id].present?
-      kid = User.find(params[:kid_id])
-      my_task = kid.my_tasks.find_by(task_id: params[:task_id])
-      puts "kid: #{my_task.user.name}"
-      puts "task: #{my_task.task.name} from #{my_task.progress}"
-      my_task.progress = "approved"
-      kid.update(score: kid.score + my_task.task.score)
-      kid.groups.where("my_groups.role = 0").each do |g|
-        g.update(score: g.score + my_task.task.score)
-        g.branch.update(score: g.branch.score + my_task.task.score)
-        g.branch.region.update(score: g.branch.region.score + my_task.task.score)
-        # g.branch.region.movement.update(score: g.branch.region.movement.score + my_task.task.score)
-      end
+			kid = User.find(params[:kid_id])
+			my_task = kid.my_tasks.find_by(task_id: params[:task_id])
+			puts "kid: #{my_task.user.name}"
+			puts "task: #{my_task.task.name} from #{my_task.progress}"
+			if params[:revert].present?
+				my_task.progress = "in_progress"
+			else
+				my_task.progress = "approved"
+				kid.update(score: kid.score + my_task.task.score)
+				kid.groups.where("my_groups.role = 0").each do |g|
+					g.update(score: g.score + my_task.task.score)
+					g.branch.update(score: g.branch.score + my_task.task.score)
+					g.branch.region.update(score: g.branch.region.score + my_task.task.score)
+					# g.branch.region.movement.update(score: g.branch.region.movement.score + my_task.task.score)
+				end
+			end
     else
-      my_task = current_user.my_tasks.find_by(task_id: params[:task_id])
-      puts "task: #{my_task.task.name} from #{my_task.progress}"
-      my_task.progress = "done"
+			my_task = current_user.my_tasks.find_by(task_id: params[:task_id])
+			if params[:remove].present?
+				my_task.delete
+			else
+				puts "task: #{my_task.task.name} from #{my_task.progress}"
+				my_task.progress = "done"
+			end
     end
-    my_task.save
+    my_task.save unless params[:remove].present?
     puts "to #{my_task.progress}"
     redirect_to root_path
   end
