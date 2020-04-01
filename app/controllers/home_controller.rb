46da9791499be73b0aa36e59439d6eba2f3edcbe
Jordan Shefner <yarden11111@gmail.com>
2020-04-01 23:18:28 +0300

    Fix stupid mistake

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
       @families = Family.all
       @houses = House.all
       @house = @houses.first
+      @family = @house.family
     else
       @houses = current_user.houses
 			if params[:house].present?
