7a455a5712874cb55d2b12e5dd821d5308fbf895
Pashi <guynatan@gmail.com>
2020-04-01 22:04:01 +0300

    new design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,14 @@ class HomeController < ApplicationController
       @families = Family.all
       @houses = House.all
     else
-      @families = current_user.families
       @houses = current_user.houses
+			if params[:house].present?
+				cookies[:house] = { value: params[:house], expires: Time.now + 36000000}
+			else
+				cookies[:house] = { value: @houses.first.id, expires: Time.now + 36000000} unless cookies[:house].present?
+			end
+			@house = House.find(cookies[:house])
+			@family = @house.family
     end
 	end
 
