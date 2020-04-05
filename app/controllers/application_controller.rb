e6b7e1a187a75f8495db8b510fa798c014300527
Jordan Shefner <yarden11111@gmail.com>
2020-04-05 13:11:46 +0300

    Add he i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
   protected
 
     def configure_permitted_parameters
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Pinkas
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    I18n.config.available_locales = :he, :en
     config.i18n.default_locale = :he
   end
 end
