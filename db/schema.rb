5aeb6eb4be01cd2449ff4c2f74957a902457bb1a
Jordan Shefner <yarden11111@gmail.com>
2020-04-05 13:26:33 +0300

    Add en and lang to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_062851) do
+ActiveRecord::Schema.define(version: 2020_04_05_102519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_04_01_062851) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "role"
+    t.string "lang"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
