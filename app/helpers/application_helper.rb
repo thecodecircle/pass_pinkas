83ee943dcf68c9da55a5a42c2d6caefb5e083a9d
Pashi <guynatan@gmail.com>
2020-04-06 17:46:06 +0300

    error btn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,7 @@ module ApplicationHelper
 	end
 
 	def error_btn
-		if mobile_device?
-			html = "<hr>"
-			html += "<a href='whatsapp://send?text=דיווח על תקלה&phone=972546621300' data-action='share/whatsapp/share' class='mt-2 block bg-yellow-500 hover:bg-yellow-700 text-black px-4 rounded flex items-center h-8'><span>#{image_tag "bug.svg", size:"15"}</span> <span class='mx-2'>דיווח על תקלה </span></a>"
-		else
-			html = mail_to 'yarden11111@gmail.com', "#{image_tag "bug.svg", size:"15"}".html_safe,class:'btn btn-default mb-2 lg:mr-2 lg:mb-0 block bg-yellow-500 text-black font-normal border-none py-2 h-10  w-12 flex items-center', subject: 'דיווח על תקלה'
-		end
+			html = mail_to 'yarden11111@gmail.com', "#{image_tag "bug.svg", size:"15"} #{I18n.t("home.report_btn")}".html_safe,class:'btn btn-default mt-2 block bg-yellow-500 text-black font-normal border-none py-2 h-6  w-full flex items-center', subject: "#{I18n.t("home.report_error")}"
 		html.html_safe
 	end
 
