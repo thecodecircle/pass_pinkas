91af1ff04cc1fe50b2e34152b75729af1d6a298a
Jordan Shefner <yarden11111@gmail.com>
2020-04-14 14:09:45 +0300

    more work on push

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -98,47 +98,25 @@ class TasksController < ApplicationController
 			@house = House.find(params[:house])
 			if @house.tasks.exclude?(@task)
         @house.tasks << @task
-        # require 'fcm'
-
-        # fcm = FCM.new("AAAAhx0k54s:APA91bFpfLtMkJr_Z69jhGr8cW7HTNJBgiHPNjykSxmL96uLUSIf47SjHchPKhP59ChqXZDy7peD52j-PfNF4qAEC2x45-IKSJXkCL88RC6UYSvggoe1b9YHqCIgKLBkvflGh9CJv4wg")
-        # you can set option parameters in here
-        #  - all options are pass to HTTParty method arguments
-        #  - ref: https://github.com/jnunemaker/httparty/blob/master/lib/httparty.rb#L29-L60
-        #  fcm = FCM.new("my_server_key", timeout: 3)
-
-        # registration_ids = ["dnJq1KM6pmHbaz7GxI6P7x:APA91bH0Ssg2_rY0avHUbbiEYGx87jKUMyLHXJogXfMmbTVZ64LhgO5OSJxD2nXM5CiGXQpunGsv-45c4rLyE2O1_FtRsM90BoIDL2KxQUWazzE4f-SPH9kjSO9tRORBsELPqp-2cf4N"] # an array of one or more client registration tokens
-        # puts "************** FCM **************************"
-        # See https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages for all available options.
-        # options = { "notification": {
-            # "title": "Portugal vs. Denmark",
-            # "body": "5 to 1"
-          # }
-        # }
-        # response = fcm.send(registration_ids, options)
         @house.users.each do |user|
           NotificationChannel.broadcast_to(user, ["#{current_user.name} איתגר אותך באתגר #{@task.name}", "#{view_context.image_url('logo.png')}", "#{view_context.image_url('badge-32.png')}", "#{view_context.asset_url('just-saying.mp3')}"])
           puts "********************  Notification to #{user.name} ********************"
         end
       else
-        NotificationChannel.broadcast_to(current_user, "ל#{@house.name} כבר יש את המשימה הזאת")
+        NotificationChannel.broadcast_to(current_user, ["ל#{@house.name} כבר יש את המשימה הזאת", "#{view_context.image_url('logo.png')}", "#{view_context.image_url('badge-32.png')}", "#{view_context.asset_url('just-saying.mp3')}"])
         puts "********************  Notification to #{current_user.name} ********************"
       end
-      # ActionCable.server.broadcast('notification_channel', "The task #{@task.name} had been assigned to #{@house.name}")
-        # NotificationChannel.broadcast_to(user, { notification: 'Test message' })
-      # NotificationChannel.broadcast_to(@house.users.first, { notification: "The task #{@task.name} had been assigned to #{@house.name}" })
 		elsif params[:family].present?
 			@family = Family.find(params[:family])
 			@family.houses.each do |house|
 				if house.tasks.exclude?(@task)
           house.tasks << @task
           house.users.each do |user|
-            NotificationChannel.broadcast_to(user, "#{current_user.name} איתגר אותך באתגר #{@task.name}")
+            NotificationChannel.broadcast_to(user, ["#{current_user.name} איתגר אותך באתגר #{@task.name}", "#{view_context.image_url('logo.png')}", "#{view_context.image_url('badge-32.png')}", "#{view_context.asset_url('just-saying.mp3')}"])
             puts "********************  Notification to #{user.name} ********************"
           end
         end
 			end
-      # ActionCable.server.broadcast('notification_channel', "The task #{@task.name} had been assigned to #{@family.name}")
-      # NotificationChannel.broadcast_to(@family.users.first, { notification: "The task #{@task.name} had been assigned to #{@family.name}" })
 		end
     redirect_to root_path
   end
